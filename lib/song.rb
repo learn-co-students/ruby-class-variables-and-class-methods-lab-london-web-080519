d655e7c05029db9078bb31e5b40f7e23e19c4aaf
PollyCR <polly.c.reynolds@gmail.com>
2019-08-06 20:57:23 +0100

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,55 +1,55 @@
 class Song
  
-   @@count= 0
-   @@artists = []
-   @@genres = []
-
-
-    attr_accessor :name, :artist, :genre 
-   
-    def initialize(name,artist,genre)
-      @name = name
-      @artist = artist
-      @genre = genre
-      @@count +=1
-
-      @@artists << @artist 
-      @@genres << @genre
-    end
-
-    def self.count 
-        return @@count 
-    end 
-
-    def self.artists 
-        return @@artists.uniq
-    end
-
-    def self.genres 
-        return @@genres.uniq 
-    end 
+    @@count= 0
+    @@artists = []
+    @@genres = []
+ 
+ 
+     attr_accessor :name, :artist, :genre 
     
-    def self.genre_count
-        genre_count = {}
-        @@genres.each do |genre|
-          if genre_count[genre] #if the genre is already in the hash
-            genre_count[genre] += 1
-          else #if it's a new genre
-            genre_count[genre] = 1
-          end
-        end
-        genre_count
-      end
-    
-      def self.artist_count
-        artist_count = {}
-        @@artists.each do |artist|
-          if artist_count[artist] #if the artist is already in the hash
-            artist_count[artist] += 1
-          else
-            artist_count[artist] = 1
-          end
-        end
-        artist_count
-      end
-    end
+     def initialize(name,artist,genre)
+       @name = name
+       @artist = artist
+       @genre = genre
+       @@count +=1
+ 
+       @@artists << @artist 
+       @@genres << @genre
+     end
+ 
+     def self.count 
+         return @@count 
+     end 
+ 
+     def self.artists 
+         return @@artists.uniq
+     end
+ 
+     def self.genres 
+         return @@genres.uniq 
+     end 
+     
+     def self.genre_count
+         genre_count = {}
+         @@genres.each do |genre|
+           if genre_count[genre] #if the genre is already in the hash
+             genre_count[genre] += 1
+           else #if it's a new genre
+             genre_count[genre] = 1
+           end
+         end
+         genre_count
+       end
+     
+       def self.artist_count
+         artist_count = {}
+         @@artists.each do |artist|
+           if artist_count[artist] #if the artist is already in the hash
+             artist_count[artist] += 1
+           else
+             artist_count[artist] = 1
+           end
+         end
+         artist_count
+       end
+     end
